ac41dfaf2d4c8cff3b64e0b766ac0a030ec02818
Serter Iyigunlu <serteri@hotmail.com>
2023-10-04 22:08:44 +1000

    questions updated

--- src/components/Question.js
+++ src/components/Question.js
@@ -14,48 +14,84 @@ function Question(props){
     const[currentQuestion,setCurrentQuestion] =useState(0)
     const questions = [
         {
-            text: "Turkiye'nin Baskenti?",
+            text: "What country has the highest life expectancy?",
             options: [
-                { id: 0, text: "Izmir", isCorrect: false },
-                { id: 1, text: "Antalya", isCorrect: false },
-                { id: 2, text: "Istanbul", isCorrect: false },
-                { id: 3, text: "Ankara", isCorrect: true },
+                { id: 0, text: "Australia", isCorrect: false },
+                { id: 1, text: "New Zealand", isCorrect: false },
+                { id: 2, text: "Turkey", isCorrect: false },
+                { id: 3, text: "Hong Kong", isCorrect: true },
             ],
         },
         {
-            text: "What year was the Constitution of America written?",
+            text: "Where would you be if you were standing on the Spanish Steps?",
             options: [
-                { id: 0, text: "1787", isCorrect: true },
-                { id: 1, text: "1776", isCorrect: false },
-                { id: 2, text: "1774", isCorrect: false },
-                { id: 3, text: "1826", isCorrect: false },
+                { id: 0, text: "Rome", isCorrect: true },
+                { id: 1, text: "Istanbul", isCorrect: false },
+                { id: 2, text: "London", isCorrect: false },
+                { id: 3, text: "Paris", isCorrect: false },
             ],
         },
         {
-            text: "Who was the second president of the US?",
+            text: "What year was the United Nations established?",
             options: [
-                { id: 0, text: "John Adams", isCorrect: true },
-                { id: 1, text: "Paul Revere", isCorrect: false },
-                { id: 2, text: "Thomas Jefferson", isCorrect: false },
-                { id: 3, text: "Benjamin Franklin", isCorrect: false },
+                { id: 0, text: "1945", isCorrect: true },
+                { id: 1, text: "1988", isCorrect: false },
+                { id: 2, text: "1948", isCorrect: false },
+                { id: 3, text: "1952", isCorrect: false },
             ],
         },
         {
-            text: "What is the largest state in the US?",
+            text: "How many minutes are in a full week?",
             options: [
-                { id: 0, text: "California", isCorrect: false },
-                { id: 1, text: "Alaska", isCorrect: true },
-                { id: 2, text: "Texas", isCorrect: false },
-                { id: 3, text: "Montana", isCorrect: false },
+                { id: 0, text: "11,050", isCorrect: false },
+                { id: 1, text: "10,080", isCorrect: true },
+                { id: 2, text: "9,850", isCorrect: false },
+                { id: 3, text: "15,874", isCorrect: false },
             ],
         },
         {
-            text: "Which of the following countries DO NOT border the US?",
+            text: "How many elements are in the periodic table?",
             options: [
-                { id: 0, text: "Canada", isCorrect: false },
-                { id: 1, text: "Russia", isCorrect: true },
-                { id: 2, text: "Cuba", isCorrect: true },
-                { id: 3, text: "Mexico", isCorrect: false },
+                { id: 0, text: "121", isCorrect: false },
+                { id: 1, text: "118", isCorrect: true },
+                { id: 2, text: "147", isCorrect: false },
+                { id: 3, text: "135", isCorrect: false },
+            ],
+        },
+        {
+            text: "Aureolin is a shade of what color?",
+            options: [
+                { id: 0, text: "Red", isCorrect: false },
+                { id: 1, text: "Blue", isCorrect: false },
+                { id: 2, text: "Yellow", isCorrect: true },
+                { id: 3, text: "Pink", isCorrect: false },
+            ],
+        },
+        {
+            text: "What country drinks the most coffee per capita?",
+            options: [
+                { id: 0, text: "Finland", isCorrect: true },
+                { id: 1, text: "Turkey", isCorrect: false },
+                { id: 2, text: "England", isCorrect: false },
+                { id: 3, text: "France", isCorrect: false },
+            ],
+        },
+        {
+            text: "Which planet has the most moons?",
+            options: [
+                { id: 0, text: "Earth", isCorrect: false },
+                { id: 1, text: "Mars", isCorrect: false },
+                { id: 2, text: "Venus", isCorrect: false },
+                { id: 3, text: "Saturn", isCorrect: true },
+            ],
+        },
+        {
+            text: "How many bones do we have in an ear?",
+            options: [
+                { id: 0, text: "5", isCorrect: false },
+                { id: 1, text: "3", isCorrect: true },
+                { id: 2, text: "12", isCorrect: false },
+                { id: 3, text: "2", isCorrect: false },
             ],
         },
     ];
